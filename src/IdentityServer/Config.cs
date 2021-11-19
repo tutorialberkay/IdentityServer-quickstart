a3bedf243ca3c0857ec5cca74b3a39ace082035f
biproberkay <biproberkay@gmail.com>
2021-11-19 15:13:00 +0300

    Initial Commit

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            { 
+                new ApiScope("api1", "My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { };
+    }
+}
